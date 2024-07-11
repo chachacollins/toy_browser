9ec28a2257bd0050ad395e0514ad00f3df3f3a0e
cursedcadaver <collinschacha@hotmail.com>
2024-07-11 18:20:19 +0300

    Implemented simple dom

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{char, collections::HashMap, string};
 
 struct Node {
     children: Vec<Node>,
@@ -7,26 +7,55 @@ struct Node {
 
 enum NodeType {
     Text(String),
-    Element(ElementData)
+    Element(ElementData),
 }
 
 struct ElementData {
     tagname: String,
     attributes: AtrrMap,
-    
 }
 type AttrMap = HashMap<String, String>;
 
 fn text(data: String) -> Node {
-    Node {children: Vec::new(), node_type: NodeType::Text(data)}
+    Node {
+        children: Vec::new(),
+        node_type: NodeType::Text(data),
+    }
 }
-fn elem(name: String, attrs: AttrMap, children: Vec<Node>) -> Node{
+fn elem(name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
     Node {
         children: children,
-        node_type: NodeType::Element(ElementData { tagname: name, attributes: attrs })
+        node_type: NodeType::Element(ElementData {
+            tagname: name,
+            attributes: attrs,
+        }),
     }
 }
 
+struct Parser {
+    pos: usize,
+    input: String,
+}
+
+impl Parser {
+    fn next_char(&self) -> char {
+        self.input[self.pos..].chars().next().unwrap()
+    }
+    fn starts_with(&self, s: &str) -> bool {
+        self.input[self.pos..].starts_with(s)
+    }
+    fn eos(&self) -> bool {
+        self.pos >= self.input.len()
+    }
+    fn consume_char(&mut self) -> char{
+        let mut iter = self.input[self.pos..].char_indices();
+        let (_, cur_char) = iter.next().unwrap();
+        let (next_pos, _) = iter.next().unwrap_or((1, ' '));
+        self.pos += self.input.len();
+        cur_char
+
+    }
+}
 
 fn main() {
     println!("Hello, world!");
